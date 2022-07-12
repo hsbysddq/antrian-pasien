d63cb4734668f0e75a4c84a1aad2005a5e93fb0b
hsbysddq <hsby15sddq@gmail.com>
2022-07-12 22:41:28 +0700

    Membuat Display Untuk Pasien

---
+++ resources/js/Pages/Display.jsx
@@ -0,0 +1,7 @@
+export default function Display() {
+    return (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+            <h1>Faskes Tingkat Lanjut Rumah Sakit Ambunten</h1>
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,10 @@ Route::get('/login1', function () {
     return Inertia::render('Login');
 });
 
+Route::get('/display', function () {
+    return Inertia::render('Display');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
